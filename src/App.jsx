5f1dde79be112081ab94719d9650dbd94ca81d7f
ssmith2006 <shantel.smith@hotmail.com>
2025-05-13 16:11:03 +0000

    defined a color theme and demonstrated nesting

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
-
-import './App.scss'
+import "./App.scss";
 
 function App() {
-  
-
   return (
     <>
-      <h1>It's a SASSY day!</h1>
+      <header>
+        <h1>It's a SASSY  day</h1>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+      </header>
+      <main>
+        <section className="section-one">
+          <h2>SOOOOO Sassy</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia odit quo rem sequi totam dolor architecto quos vero! Ratione, magnam laboriosam voluptatem quasi blanditiis itaque reprehenderit pariatur eos nemo minima.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime atque consequatur omnis aspernatur, dignissimos soluta sint reprehenderit voluptatum, perferendis ratione error nemo voluptatibus quos iste asperiores quo accusamus tempore voluptas.
+          Eaque perferendis eum, id qui veritatis debitis ad nemo. Placeat nobis beatae soluta at atque exercitationem ipsa provident sequi distinctio quaerat? Temporibus nihil, in ut aperiam atque similique consectetur a!</p>
+        </section>
+        <section className="section-two">
+          <h2>We Are Sassy</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, quidem? Et dolores dolorum inventore autem enim! Nemo vel eos at labore. Voluptatem voluptatibus cupiditate odio neque inventore similique totam quam.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quidem aperiam, consequuntur culpa quos inventore! Reiciendis, quos amet sunt ducimus quo non. Assumenda, repellat adipisci dolorum odit impedit atque vel!
+          Nostrum maxime maiores libero magnam distinctio repellat iste voluptate vel praesentium dicta voluptates eum iusto sunt, animi ad fugiat laudantium! Totam, veritatis perferendis! Consequuntur corrupti inventore debitis ab consequatur dolore.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident earum iure voluptatum, dolorem quam qui omnis deleniti fugit labore, molestiae minima velit ullam? Eligendi totam inventore, voluptas nemo pariatur officiis?
+          Ut optio dolor magnam. Nisi ducimus architecto voluptate. Tempore rerum quos, excepturi reprehenderit consequuntur sit sint commodi nemo. Vero repellat consectetur quos cupiditate quam minus molestias ut, quis recusandae earum.
+          Rem consequatur, incidunt rerum quia nemo veniam corrupti ut. Dolores et natus corrupti ut architecto nulla quasi quam perferendis temporibus voluptatem facilis reiciendis magnam provident, veniam ducimus quidem sint ex.</p>
+        </section>
+        <section className="section-three">
+          <h2>We Need Your Sassy Ear</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit blanditiis optio illum! Explicabo dignissimos quibusdam exercitationem beatae fugit ullam quaerat, unde excepturi saepe nemo officia molestiae eos iste, neque animi?
+          Vero, fugiat, totam labore nisi beatae id in suscipit et dolor saepe ab modi, praesentium animi similique ut veritatis magni! Tempore perferendis aliquam omnis cum non rem quos accusamus mollitia.
+          Veniam quas neque animi, adipisci placeat facere beatae hic, velit error amet facilis possimus numquam ad, atque molestias aperiam deserunt necessitatibus? Debitis repudiandae optio enim dignissimos aspernatur dolorem facilis sint.</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet aperiam ut excepturi obcaecati beatae totam, sapiente, dolore repudiandae porro ipsa nobis. Corrupti doloremque ad cumque excepturi, ab et consectetur ea.</p>
+        </section>
+      </main>
+      <footer><p>&copy; 2025 We Seaux Sassy. All Rights Reserved</p></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
